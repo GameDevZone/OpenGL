8b9590a6be1d829f89ce093fe1bdc1a9bfdba4c6
zhyrao <rain330047@outlook.com>
2018-12-20 15:26:13 +0800

    Add SetMat4 function in Shader file

--- OpenGL/src/Shader.h
+++ OpenGL/src/Shader.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <glad/glad.h>
 
+#include "glm.hpp"
+#include "gtc/matrix_transform.hpp"
+#include "gtc/type_ptr.hpp"
+
 #include <string>
 #include <fstream>
 #include <sstream>
@@ -85,6 +89,10 @@ public:
 		glUniform1f(glGetUniformLocation(ShaderID, name.c_str()), value);
 	}
 
+	void SetMat4(const std::string &name, glm::mat4 value) const
+	{
+		glUniformMatrix4fv(glGetUniformLocation(ShaderID, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
+	}
 private:
 	void checkCompileErrors(unsigned int shader, std::string type)
 	{
