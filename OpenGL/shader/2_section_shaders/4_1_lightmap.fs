1d5e0a40e9fe2217c7d227e6d6c1c0eea94306c3
zhyrao <rain330047@outlook.com>
2018-12-21 16:09:17 +0800

    Diffuse Lighting Map

---
+++ OpenGL/shader/2_section_shaders/4_1_lightmap.fs
@@ -0,0 +1,51 @@
+#version 330 core
+out vec4 FragColor;
+
+struct Material{
+	sampler2D diffuseTexture;
+
+	vec3 specular;
+	float shininess;
+};
+  
+uniform Material material;
+
+struct Light{
+	vec3 position;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
+uniform Light light;
+
+uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform vec3 camPos;
+
+in vec3 normal;
+in vec3 modelPos;
+in vec2 Texcoord;
+
+void main()
+{
+	// ambient color
+	vec3 ambientColor = light.ambient * vec3(texture(material.diffuseTexture, Texcoord));
+
+	// diffuse color 
+	vec3 norm = normalize(normal);
+	vec3 lightDir = normalize(lightPos - modelPos);
+	float diffusePower = max(dot(norm, lightDir), 0);
+	vec3 diffuseColor = diffusePower * light.diffuse * vec3(texture(material.diffuseTexture, Texcoord));
+
+	// specular
+	vec3 viewDir = normalize(camPos - modelPos);
+	vec3 reflectDir = reflect(-lightDir, normal);
+	float specPow = pow(max(dot(reflectDir, viewDir), 0.0), material.shininess);
+	vec3 specularColor = specPow * material.specular * light.specular;
+
+
+    FragColor = vec4((ambientColor  + diffuseColor + specularColor), 1.0);
+}
+
